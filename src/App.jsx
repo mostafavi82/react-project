66804e3dd7913d34be95b60c0a7de5d20b628210
s.h.m <s.h.mostafavi82@gmail.com>
2024-10-05 13:15:11 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,41 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // <>
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </>
+    <div className=''>
+    <header>
+       <h3>Starweather</h3>
+       <div className="">
+         <select >
+          <option value={'Tehran'}>tehran</option>
+          <option value={'OtherCity'}>otherCity</option>
+          <label className='lableBox'>انتخاب شهر</label>
+         </select>
+       </div>
+    </header>
+   
+ </div>
   )
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,35 @@
 import Footer from "./Footer.jsx"
 import Course from "./Course.jsx"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Doing from "./Doing.jsx"
 import Todos from "./Todos.jsx"
+import App from "./App"
 
 export default function Home() {
+    // const [books, setBooks] = useState([])
+    // useEffect(()=> {
+    //     fetch('https://api.itbook.store/1.0/search/confidence')
+    //     .then((res)=>{
+    //         return res.json();
+    //     },  )
+    //     .then((data)=>{
+    //         console.log(data);
+    //         setBooks(data.books);
+    //     })
+    // }, [])
 
 
     return (
 
-        <div className="bg-gray-100">
-            <Todos />
-            <h1 className=' '>salam</h1>
-        </div>
+        //   <div className="flex flex-wrap">
+        //     {/* {books.map((book)=>(
+        //         <div className="w-[24%] flex flex-col items-center ">
+        //             <img src={book.image} alt="" />
+        //             <p className="text-center ">{book.title} </p>
+        //         </div>
+        //     ))} */}
+        //   </div>
+        <App/>
 
-
-    )
+    );
 }
